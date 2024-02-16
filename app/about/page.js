a478932835538b25ece91757d94ef2ab52ba34b0
Julia <jules.rechi@gmail.com>
2024-02-16 16:45:00 +0100

    Add About page

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,10 @@
+import React from 'react';
+
 export const metadata = {
   title: 'About page',
   description: 'This page shows the about page',
 };
+
+export default function AboutPage() {
+  return <div>This is my about page</div>;
+}
--- app/contact/page.js
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'Contact page',
-  description: 'This page shows all contact possibilities',
-};
