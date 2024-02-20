cdbf97cabd43f325fbc3c56ccf01cbaa9f4484a6
Julia <jules.rechi@gmail.com>
2024-02-20 15:57:10 +0100

    Change of thank you page title

--- app/checkout/thankyou/page.js
+++ app/checkout/thankyou/page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './page.module.scss';
 
+export const metadata = {
+  title: 'Thank you page',
+  description: 'This page appears after a successful order',
+};
+
 export default function thankYouPage() {
   return (
     <div className={styles.sectionContainer}>
       <div>
-        <h1>Thank you for your order!</h1>
+        <h1>Thank you for your order</h1>
       </div>
       <div className={styles.thankyouText}>
         Your order has been recieved and a confirmation has been sent to your
