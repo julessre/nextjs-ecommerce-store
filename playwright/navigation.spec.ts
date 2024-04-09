94c76ddfb192e0727b54198876e131f42e7ce750
Julia <jules.rechi@gmail.com>
2024-04-09 10:29:04 +0200

    Add Navgiation Playwright Test

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('navigation test', async ({ page }) => {
   await page.goto('https://nextjs-ecommerce-store-2024.fly.dev/');
 
   await expect(
@@ -8,4 +8,14 @@ test('has title', async ({ page }) => {
       name: ' Immerse Yourself: Nurture Your Mind, Body and Soul',
     }),
   ).toBeVisible();
+
+  await expect(
+    page.getByRole('img', { name: 'yoga place' }).first(),
+  ).toBeVisible();
+
+  await page.getByRole('link', { name: 'Workshops' }).click();
+  await page.waitForURL(
+    'https://nextjs-ecommerce-store-2024.fly.dev/workshops',
+  );
+  await expect(page.getByText('Upcoming workshops')).toBeVisible();
 });
