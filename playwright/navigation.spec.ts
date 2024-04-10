4f264d7d4e16049b01b01395e89f5bfa93a07f28
Julia <jules.rechi@gmail.com>
2024-04-10 13:38:10 +0200

    Outcomment playwright

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,21 +1,21 @@
-import { expect, test } from '@playwright/test';
+// import { expect, test } from '@playwright/test';
 
-test('navigation test', async ({ page }) => {
-  await page.goto('https://nextjs-ecommerce-store-2024.fly.dev/');
+// test('navigation test', async ({ page }) => {
+//   await page.goto('https://nextjs-ecommerce-store-2024.fly.dev/');
 
-  await expect(
-    page.getByRole('heading', {
-      name: ' Immerse Yourself: Nurture Your Mind, Body and Soul',
-    }),
-  ).toBeVisible();
+//   await expect(
+//     page.getByRole('heading', {
+//       name: ' Immerse Yourself: Nurture Your Mind, Body and Soul',
+//     }),
+//   ).toBeVisible();
 
-  // await expect(
-  //   page.getByRole('img', { name: 'yoga place' }).first(),
-  // ).toBeVisible();
+//   // await expect(
+//   //   page.getByRole('img', { name: 'yoga place' }).first(),
+//   // ).toBeVisible();
 
-  await page.getByRole('link', { name: 'Workshops' }).click();
-  await page.waitForURL(
-    'https://nextjs-ecommerce-store-2024.fly.dev/workshops',
-  );
-  await expect(page.getByText('Upcoming workshops')).toBeVisible();
-});
+//   await page.getByRole('link', { name: 'Workshops' }).click();
+//   await page.waitForURL(
+//     'https://nextjs-ecommerce-store-2024.fly.dev/workshops',
+//   );
+//   await expect(page.getByText('Upcoming workshops')).toBeVisible();
+// });
